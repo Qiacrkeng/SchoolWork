00594f1e45dca3d83346788c7f9213aa027f1a94
Taoer <Qiacrkeng@gamil.com>
2022-05-24 11:18:38 +0800

    課堂練習

---
+++ java/l4_2022_5_24.java
@@ -0,0 +1,26 @@
+public class l4_2022_5_24 {
+    public static void main(String[] args) {
+        String words = "Hello World Java 11";
+        String[] result = new String[words.length()];
+        int count = 0, left = 0;
+        for (int right = 0; right < words.length(); right++) {
+            if (words.charAt(right) == ',' || words.charAt(right) == ' ' || words.charAt(right) == '!') {
+                result[count] = words.substring(left, right);
+                count++;
+                left = right + 1;
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count - i - 1; j++) {
+                if (result[j].codePointAt(0) > result[j + 1].codePointAt(0)) {
+                    String temp = result[j];
+                    result[j] = result[j + 1];
+                    result[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(result[i]);
+        }
+    }
+}
